4eaa133bf8ce1adc554a29f80fd5f4460039e8d9
somiljain2006 <somil16022006@gmail.com>
2026-07-18 15:29:17 +0530

    Initial Commit

---
+++ Friends-District/Friends_DistrictApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friends_DistrictApp.swift
+//  Friends-District
+//
+//  Created by somil jain on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Friends_DistrictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
